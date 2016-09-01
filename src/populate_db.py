ca6e134d8aeec6ee46b94ff26f758b454a29cc1b
Mateus Pinto Rodrigues <mprodrigues@openmailbox.org>
2016-09-01 14:27:16 -0300

    Coluna estado agora é do tipo INTEGER e não mais NUMERIC

--- src/populate_db.py
+++ src/populate_db.py
@@ -1,15 +1,16 @@
 #! /usr/bin/python2
 
+import os
 import sqlite3
 
-conn = sqlite3.connect("/home/mprodrigues/.housekeeping/hk.db")
+home = os.getenv("HOME")
+conn = sqlite3.connect(home + "/.housekeeping/hk.db")
 c = conn.cursor()
 
 c.execute('''CREATE TABLE aparelhos
-(nome text, kwh real, valor real, estado numeric, PRIMARY KEY(nome))''')
+(nome text, kwh real, valor real, estado integer, PRIMARY KEY(nome))''')
 c.execute('''CREATE TABLE historico
 (nome text, mes integer, valor real, FOREIGN KEY(nome) REFERENCES aparelhos(nome))''')
 
 conn.commit()
-c.execute('''select * from aparelhos''')
 conn.close()
