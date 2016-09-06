a01af0d87f428ed46be5c54f6a617a3787bd47d1
Mateus Pinto Rodrigues <mprodrigues@openmailbox.org>
2016-09-06 19:59:50 -0300

    Retira mensagem de debug de shistory.py

--- src/shistory.py
+++ src/shistory.py
@@ -10,7 +10,7 @@ c = conn.cursor()
 
 c.execute('''SELECT * FROM historico''')
 records = c.fetchall()
-print records
+
 f = open(home + "/.housekeeping/historico.json", "w")
 
 f.write(json.dumps(records))
