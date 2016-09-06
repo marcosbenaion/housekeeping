9258b8d120c2bbd2de73050ca578571e1c92734e
Mateus Pinto Rodrigues <mprodrigues@openmailbox.org>
2016-09-06 19:11:28 -0300

    Script de monitoramento agora funciona para qualquer dispositivo
    ex: monitor.py tv
        monitor.py ar_condicionado

--- src/monitor.py
+++ src/monitor.py
@@ -2,17 +2,21 @@
 
 import os
 import sqlite3
+import sys
 import time
 
-cost = 0.30
 total = 0
 t_time = 0
+device = (sys.argv[1],)
 
 home = os.getenv("HOME")
 conn = sqlite3.connect(home + "/.housekeeping/hk.db")
 c = conn.cursor()
 
-c.execute('''select estado from aparelhos where nome="tv"''')
+c.execute('''select kwh from aparelhos where nome=?''', device)
+kwh = c.fetchone()[0]
+cost = kwh/3600.0 * 0.55/3600.0
+c.execute('''select estado from aparelhos where nome=?''', device)
 status = c.fetchone()[0]
 
 start_time = time.time()
@@ -21,16 +25,16 @@ i = 1
 while status != 0:
     time.sleep(start_time + i*interval - time.time())
     total += cost
-    c.execute('''select estado from aparelhos where nome="tv"''')
+    c.execute('''select estado from aparelhos where nome=?''', device)
     status = c.fetchone()[0]
     t_time += 1
     i+=1
     if (t_time % 60) == 0:
-        t = (total,)
-        c.execute('''update aparelhos set valor=? where nome="tv"''', t)
+        t = (total, sys.argv[1])
+        c.execute('''update aparelhos set valor=? where nome=?''', t)
         conn.commit()
 
-t = (total,)
-c.execute('''update aparelhos set valor=? where nome="tv"''', t)
+t = (total, sys.argv[1])
+c.execute('''update aparelhos set valor=? where nome=?''', t)
 conn.commit()
 conn.close()
