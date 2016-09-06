e7afd1e096035c6a2b27139f510b7a8ce4946029
Mateus Pinto Rodrigues <mprodrigues@openmailbox.org>
2016-09-06 19:18:01 -0300

    Pequena correcao

--- src/register.py
+++ src/register.py
@@ -8,8 +8,8 @@ home = os.getenv("HOME")
 conn = sqlite3.connect(home + "/.housekeeping/hk.db")
 c = conn.cursor()
 
-dados = (sys.argv[1], sys.argv[2], sys.argv[3])
-c.execute('''INSERT INTO aparelhos VALUES(?, ?, ?, 0)''', dados) #na insercao o estado original sempre sera off, isto e, 0.
+dados = (sys.argv[1], sys.argv[2])
+c.execute('''INSERT INTO aparelhos VALUES(?, ?, 0, 0)''', dados) #na insercao o estado original sempre sera off e o valor 0.
 
 conn.commit()
 conn.close()
